b06c327c592be9e3d45d1bcf9ed6f7fafdb163d1
yujin <ruy203@gmail.com>
2024-03-18 10:01:27 +0900

    프로젝트 생성

---
+++ WeatherApp/WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Hong yujin on 3/18/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
